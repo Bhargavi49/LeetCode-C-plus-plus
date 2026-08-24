d8ab69d18ff433e750ca65ab866aa4f0d7196552
Bhargavi Deshpande <75205619+Bhargavi49@users.noreply.github.com>
2026-08-25 01:13:32 +0530

    Time: 0 ms (100%), Space: 13.5 MB (98.69%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,8 +1,26 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        target= lower_bound(nums.begin(), nums.end(), target) - nums.begin();
-      return target; 
+
+        // can be directly used shortcuts for coding for fidning lower bound
+    //     target= lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+    //   return target; 
+
+    int n= nums.size();
+    int low=0;
+    int high= n-1;
+    int ans=n;// = n because if theres nothing it'll assume as last index and return that ok
+    while(low<=high){
+        int mid= (low+high)/2;
+        if(nums[mid]>= target){
+            ans= mid;
+            high= mid -1;
+        }
+        else{
+            low= mid+1;
+        }
+    }
+    return ans;
     }
     
 };
